14c59ee151ffcb7a4868a9d53ecfdbc2882181a4
Joshua <metatron1495@gmail.com>
2021-04-06 21:05:57 -0300

    Mi primer Hola mundo

---
+++ sitaxisBasica.cpp
@@ -0,0 +1,8 @@
+// #INCLUDE NOS PERMITE AGREGAR LAS LIBRERIAS A UTILIZAR EN EL PROGRAMA
+// <iostream> es acrónicmo de Input Output Stream, libreria que nos permitirá introducir datos y ejecutarlos de salida
+#include <iostream>
+ 
+int main() {   // con int main() declaramos la función
+    std::cout << "Hola mundo"; // Aquí estamos declarando las acciones a realizar por la función
+    return 0;
+}
